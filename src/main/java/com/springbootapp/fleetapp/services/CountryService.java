34a5cebb305e292f6d65e8bb7f113c0102afee87
usc-rrr005 <rrr005@student.usc.edu.au>
2020-11-22 15:21:28 +1000

    added editModal

--- src/main/java/com/springbootapp/fleetapp/services/CountryService.java
+++ src/main/java/com/springbootapp/fleetapp/services/CountryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
@@ -19,7 +20,13 @@ public class CountryService {
     }
 
     public void save(Country country) {
+
         countryRepository.save(country);
+    }
+
+    //get by id, the optional means it will still work without crashing the program if it doesn't find the country
+    public Optional<Country> findById(int id) {
+        return countryRepository.findById(id);
     }
 
 }
